c38727779c2d8d02a02f7f679b7556759d492d7d
egipsinargo <egipsinargo123@gmail.com>
2025-01-07 21:31:08 +0700

    modelling driver table

---
+++ server/models/Driver.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Driver = sequelize.define("Driver", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    licenseNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Driver;
+};
