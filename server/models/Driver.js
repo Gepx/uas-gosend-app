8f01e915045b37293f60711f0a5b851c861febf1
egipsinargo <egipsinargo123@gmail.com>
2025-01-10 23:23:26 +0700

    make driver table

--- server/models/Driver.js
+++ server/models/Driver.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Driver = sequelize.define("Driver", {
+  const Driver = sequelize.define("driver", {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
@@ -8,14 +8,56 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
     },
-    licenseNumber: {
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     phoneNumber: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        is: /^\+?[\d\s-]+$/,
+      },
+    },
+    licensePlate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 20],
+      },
+    },
+    licenseNumber: {
+      type: DataTypes.STRING,
+    },
+    vehicleType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    yearExperience: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    profileImage: {
+      type: DataTypes.STRING, // URL to the image
+      allowNull: true,
     },
   });
   return Driver;
