70bf5500bfffb03bbd753d18313b4adba6748bae
egipsinargo <egipsinargo123@gmail.com>
2025-01-11 11:40:44 +0700

    make some change for address state management

--- client/src/store/addressStore.js
+++ client/src/store/addressStore.js
@@ -1,21 +1,68 @@
 import { create } from "zustand";
+import addressService from "../services/addressService";
 
 const useAddressStore = create((set) => ({
   addresses: [],
-  addAddress: (address) =>
-    set((state) => ({
-      addresses: [...state.addresses, { ...address, id: Date.now() }],
-    })),
-  updateAddress: (id, updatedAddress) =>
-    set((state) => ({
-      addresses: state.addresses.map((address) =>
-        address.id === id ? { ...updatedAddress, id } : address
-      ),
-    })),
-  deleteAddress: (id) =>
-    set((state) => ({
-      addresses: state.addresses.filter((address) => address.id !== id),
-    })),
+  loading: false,
+  error: null,
+
+  // Fetch all addresses
+  fetchAddresses: async () => {
+    set({ loading: true });
+    try {
+      const data = await addressService.getAllAddresses();
+      set({ addresses: data, loading: false });
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
+
+  // Add new address
+  addAddress: async (addressData) => {
+    set({ loading: true });
+    try {
+      const newAddress = await addressService.addAddress(addressData);
+      set((state) => ({
+        addresses: [...state.addresses, newAddress],
+        loading: false,
+      }));
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
+
+  // Update address
+  updateAddress: async (id, addressData) => {
+    set({ loading: true });
+    try {
+      const updatedAddress = await addressService.updateAddress(
+        id,
+        addressData
+      );
+      set((state) => ({
+        addresses: state.addresses.map((address) =>
+          address.id === id ? updatedAddress : address
+        ),
+        loading: false,
+      }));
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
+
+  // Delete address
+  deleteAddress: async (id) => {
+    set({ loading: true });
+    try {
+      await addressService.deleteAddress(id);
+      set((state) => ({
+        addresses: state.addresses.filter((address) => address.id !== id),
+        loading: false,
+      }));
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
 }));
 
 export default useAddressStore;
