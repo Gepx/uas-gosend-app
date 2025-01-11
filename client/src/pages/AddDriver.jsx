92023b49bdfd094e180a02d79eb0f0d87a969901
egipsinargo <egipsinargo123@gmail.com>
2025-01-11 21:03:54 +0700

    change props from experience to yearExperience

--- client/src/pages/AddDriver.jsx
+++ client/src/pages/AddDriver.jsx
@@ -15,7 +15,7 @@ function AddDriver() {
     licensePlate: "",
     motorbikeType: "",
     email: "",
-    experience: "",
+    yearExperience: "",
     address: "",
     profileImage: null,
     licenseNumber: "",
