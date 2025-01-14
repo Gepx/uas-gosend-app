25d3b35999c180f2682d94d9f3e7f70568da3c6f
egipsinargo <egipsinargo123@gmail.com>
2025-01-14 16:40:20 +0700

    handle state management for voucher, add some state functionality

--- client/src/store/voucherStore.js
+++ client/src/store/voucherStore.js
@@ -1,35 +1,88 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import voucherService from "../services/voucherService";
 
-const useVoucherStore = create((set, get) => ({
-  vouchers: [],
-  selectedCategory: "All",
-  voucherCode: "",
-  loading: false,
-  error: null,
+const useVoucherStore = create(
+  persist(
+    (set, get) => ({
+      vouchers: [],
+      selectedCategory: "All",
+      voucherCode: "",
+      loading: false,
+      error: null,
+      appliedVoucher: null,
+      deliveryCost: 0,
+      userVouchers: [],
 
-  setSelectedCategory: (category) => set({ selectedCategory: category }),
-  setVoucherCode: (code) => set({ voucherCode: code }),
+      setSelectedCategory: (category) => set({ selectedCategory: category }),
+      setVoucherCode: (code) => set({ voucherCode: code }),
+      setDeliveryCost: (cost) => set({ deliveryCost: cost }),
 
-  fetchVouchers: async () => {
-    set({ loading: true });
-    try {
-      const data = await voucherService.getAllVouchers();
-      set({ vouchers: data, loading: false });
-    } catch (error) {
-      set({ error: error.message, loading: false });
+      addUserVoucher: (voucher) => {
+        const currentUserVouchers = get().userVouchers;
+        if (!currentUserVouchers.some((v) => v.code === voucher.code)) {
+          set({ userVouchers: [...currentUserVouchers, voucher] });
+        }
+      },
+
+      applyVoucher: (voucher) => {
+        const currentDate = new Date();
+        const validUntil = new Date(voucher.validUntil);
+        const deliveryCost = get().deliveryCost;
+
+        if (currentDate > validUntil) {
+          throw new Error("Voucher has expired");
+        }
+
+        if (deliveryCost < voucher.minPurchase) {
+          throw new Error(
+            `Minimum purchase amount is Rp ${voucher.minPurchase.toLocaleString()}`
+          );
+        }
+
+        set({ appliedVoucher: voucher });
+      },
+
+      removeVoucher: () => {
+        set({ appliedVoucher: null });
+      },
+
+      resetVoucherState: () => {
+        set({
+          appliedVoucher: null,
+          deliveryCost: 0,
+        });
+      },
+
+      fetchVouchers: async () => {
+        set({ loading: true });
+        try {
+          const data = await voucherService.getAllVouchers();
+          set({ vouchers: data, loading: false });
+        } catch (error) {
+          set({ error: error.message, loading: false });
+        }
+      },
+
+      getFilteredVouchers: () => {
+        const { vouchers, selectedCategory } = get();
+        if (selectedCategory === "All") return vouchers;
+        return vouchers.filter(
+          (voucher) => voucher.category === selectedCategory
+        );
+      },
+
+      submitVoucherCode: () => {
+        set({ voucherCode: "" });
+      },
+    }),
+    {
+      name: "voucher-storage",
+      partialize: (state) => ({
+        userVouchers: state.userVouchers,
+      }),
     }
-  },
-
-  getFilteredVouchers: () => {
-    const { vouchers, selectedCategory } = get();
-    if (selectedCategory === "All") return vouchers;
-    return vouchers.filter((voucher) => voucher.category === selectedCategory);
-  },
-
-  submitVoucherCode: () => {
-    set({ voucherCode: "" });
-  },
-}));
+  )
+);
 
 export default useVoucherStore;
