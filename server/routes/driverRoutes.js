fa7721f0bf2ba7da6048c21385c6b67cfd7176c5
egipsinargo <egipsinargo123@gmail.com>
2025-01-18 13:35:54 +0700

    add route for post and update driver data img

--- server/routes/driverRoutes.js
+++ server/routes/driverRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const driverController = require("../controllers/driverController");
+const upload = require("../middleware/uploadMiddleware");
 
 router.get("/", driverController.getAllDrivers);
 router.get("/:id", driverController.getDriver);
-router.post("/", driverController.createDriver);
-router.put("/:id", driverController.updateDriver);
+router.post("/", upload.single("profileImage"), driverController.createDriver);
+router.put(
+  "/:id",
+  upload.single("profileImage"),
+  driverController.updateDriver
+);
 router.delete("/:id", driverController.deleteDriver);
 
 module.exports = router;
